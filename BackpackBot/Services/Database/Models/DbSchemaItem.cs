028a04a3017f70bad23b2ae25e0554ef48b5f02e
Kuromu <kuromuuuu@gmail.com>
2018-01-31 09:19:54 -0600

    steamwebapi2 scratchpad work
    adding schema to the database soon(tm)

---
+++ BackpackBot/Services/Database/Models/DbSchemaItem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackpackBot.Services.Database.Models
+{
+    public class DbSchemaItem
+    {
+    }
+}
---
+++ BackpackBot/Services/Scheduler/DbSchemaUpdater.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackpackBot.Services.Scheduler
+{
+    using FluentScheduler;
+    using NLog;
+    using SteamWebAPI2.Utilities;
+    using SteamWebAPI2.Interfaces;
+    using SteamWebAPI2.Models;
+
+    public class DbSchemaUpdater : IJob
+    {
+        private static BotConfig config = new BotConfig();
+        private static Logger log = LogManager.GetCurrentClassLogger();
+        private static EconItems items = new EconItems(config.SteamApiKey, EconItemsAppId.TeamFortress2);
+
+        public async void Execute()
+        {
+            var temp = await items.GetSchemaForTF2Async().ConfigureAwait(false);
+        }
+    }
+}
