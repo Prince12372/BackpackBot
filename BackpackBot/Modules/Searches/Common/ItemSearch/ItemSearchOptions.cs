0d304f498409e4874b7e353d184511a075b5efe7
Chrome Artiste <kuromuuuu@gmail.com>
2018-01-30 21:07:44 -0600

    Work on command line args for pricecheck command

---
+++ BackpackBot/Modules/Searches/Common/ItemSearch/ItemSearchOptions.cs
@@ -0,0 +1,19 @@
+﻿namespace BackpackBot.Modules.Searches.Common.ItemSearch
+{
+    using CommandLine;
+
+    public class ItemSearchOptions
+    {
+        [Option('q', "quality", Required = false, Default = "Genuine", HelpText = "The quality of the item (Genuine, Strange, Unusual)")]
+        public string Quality { get; set; } = "Genuine";
+
+        [Option('p', "priceindex", Required = false, Default = "0", HelpText = "The price index for the item - can be an effect name or crate series")]
+        public string PriceIndex { get; set; } = "0";
+
+        [Option('c', "craftable", Required = false, Default = true, HelpText = "Whether to search for the Craftable or Non-Craftable variant of an item")]
+        public bool Craftable { get; set; } = true;
+
+        [Option('a', "australium", Required = false, Default = false, HelpText = "Whether to search for the Australium variant of a weapon (if it exists)")]
+        public bool Australium { get; set; } = false;
+    }
+}
